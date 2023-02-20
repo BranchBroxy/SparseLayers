421ebfb3777ac30599d116253cacdb703e04ff93
BioMemsBroxy <philipp.steigerwald72@gmail.com>
2023-02-20 17:14:42 +0100

    update 20.02

--- handle_model.py
+++ handle_model.py
@@ -25,6 +25,9 @@ class handle_model():
         self.train_dataloader = train_dataloader
         self.test_dataloader = test_dataloader
         self.model = model
+        self.training_acc = []
+        self.training_loss = []
+        self.name_of_model = model.__class__.__name__
 
     def run(self):
         """
@@ -37,8 +40,8 @@ class handle_model():
         optimizer = torch.optim.Adam(self.model.parameters(), lr=1e-3)
         self.epochs = 5
         print(f"Running model")
-        for self.t in range(self.epochs):
-            print(f"Epoch {self.t + 1}\n-------------------------------")
+        for self.epoch in range(self.epochs):
+            print(f"Epoch {self.epoch + 1}\n-------------------------------")
             self.train(self.train_dataloader, self.model, loss_fn, optimizer)
             self.test(self.test_dataloader, self.model, loss_fn)
         print("Done!")
@@ -72,9 +75,7 @@ class handle_model():
             optimizer.step()
 
             if (batch + 1) % 1000 == 0:
-                epoch = 1
-                num_epochs = 1
-                print(f'Epoch [{self.t + 1}/{ self.epochs}], Step [{batch + 1}/{n_total_steps}], Loss: {loss.item():.4f}')
+                print(f'Epoch [{self.epoch + 1}/{ self.epochs}], Step [{batch + 1}/{n_total_steps}], Loss: {loss.item():.4f}')
                 #print(model.sl2.weight)
                 #print(model.sl2.connections)
 
@@ -110,3 +111,21 @@ class handle_model():
         test_loss /= num_batches
         correct /= size
         print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+        self.training_acc.append([self.epoch+1, 100*correct]),
+
+    def plot_training_acc(self, save_string="training_acc_plot.png"):
+        import pandas as pd
+        import seaborn as sns
+        import matplotlib.pyplot as plt
+        import matplotlib.ticker as ticker
+        figure, axes = plt.subplots()
+        axes.xaxis.set_major_locator(ticker.MultipleLocator(1))
+        plt.grid()
+        axes.xaxis.set_major_formatter(ticker.ScalarFormatter())
+        plt.ylabel("Accuracy in %")
+        plt.xlabel("Epochs")
+        plt.title("Accuracy comparison of NN with MNIST")
+        df = pd.DataFrame(data=self.training_acc, columns=["Epoch", "Accuracy"])
+        sns.lineplot(data=df, x=df.Epoch, y=df.Accuracy, ax=axes)
+        plt.savefig(save_string)
+        plt.close()
--- main.py
+++ main.py
@@ -10,20 +10,90 @@ def init_function():
     print(torch.cuda.current_device())
     print(torch.cuda.device_count())
     print(torch.cuda.get_device_name(0))
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    #device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print('Using device:', device)
 
+
+
+def create_dataframe(data):
+    import pandas as pd
+    # Erstelle einen leeren DataFrame
+    df = pd.DataFrame()
+    # Erstelle die restlichen Spalten mit den zweiten Elementen
+    for sublist in data:
+        df[sublist[0]] = sublist[0:]
+    column_names = ["Epoch"]
+    for i in range(len(data)):
+        string_to_append = "Acc" + str(i+1)
+        column_names.append(string_to_append)
+    df.columns = column_names
+    return df
+
+def compare_acc(acc_list, save_string="Acc_compare.png"):
+    import pandas as pd
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+    import matplotlib.ticker as ticker
+
+    figure, axes = plt.subplots()
+    axes.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    plt.grid()
+    axes.xaxis.set_major_formatter(ticker.ScalarFormatter())
+    plt.ylabel("Accuracy in %")
+    plt.xlabel("Epochs")
+    plt.title("Accuracy comparison of NN with MNIST")
+    df = create_dataframe(acc_list)
+    # df = pd.DataFrame(data=acc_list, columns=["Epoch", "Accuracy"])
+    sns.lineplot(data=df, x=df.Epoch, y=df.Acc1, ax=axes, label="Dense")
+    sns.lineplot(data=df, x=df.Epoch, y=df.Acc2, ax=axes, label="Sparse")
+    plt.legend(loc='lower right')
+    # axes.legend(labels=["Acc1", "Acc2"])
+    plt.savefig(save_string)
+    plt.close()
+
+def compare_models_robustness(mode: str, *models: nn.Module) -> None:
+    from handle_model import handle_model
+    acc_list_per_noise_level = []
+    noise_levels = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+    for noise_level in noise_levels:
+        print(noise_level)
+        noisy_train_dataset = add_noise_to_mnist_dataset(train_dataloader.dataset, noise_level=noise_level)
+        noisy_train_loader = torch.utils.data.DataLoader(dataset=noisy_train_dataset, batch_size=10, shuffle=False)
+
+        noisy_test_dataset = add_noise_to_mnist_dataset(test_dataloader.dataset, noise_level=noise_level)
+        noisy_test_loader = torch.utils.data.DataLoader(dataset=noisy_test_dataset, batch_size=10, shuffle=False)
+
+        model_handlers = []
+        for model in models:
+            model_handlers.append(handle_model(model, noisy_train_loader, noisy_test_loader))
+            #models[0].__class__.__name__
+        acc_list = []
+        for model_runner in model_handlers:
+            model_runner.run()
+            acc_list.append(model_runner.training_acc[-1][-1])
+
+        list_to_append = []
+        list_to_append.append(noise_level)
+        for acc in acc_list:
+            list_to_append.append(acc)
+
+
+        acc_list_per_noise_level.append(list_to_append)
+
+    print(acc_list_per_noise_level)
 if __name__ == '__main__':
-    init_function()
+    #init_function()
     from get_data import load_mnist, add_noise_to_mnist_dataset
     batch_size = 10
     train_dataloader, test_dataloader = load_mnist(batch_size=batch_size)
 
-    noisy_train_dataset = add_noise_to_mnist_dataset(train_dataloader.dataset, noise_level=0.9)
+    noisy_train_dataset = add_noise_to_mnist_dataset(train_dataloader.dataset, noise_level=0.25)
     noisy_train_loader = torch.utils.data.DataLoader(dataset=noisy_train_dataset, batch_size=10, shuffle=False)
 
-    noisy_test_dataset = add_noise_to_mnist_dataset(test_dataloader.dataset, noise_level=0.9)
+    noisy_test_dataset = add_noise_to_mnist_dataset(test_dataloader.dataset, noise_level=0.25)
     noisy_test_loader = torch.utils.data.DataLoader(dataset=noisy_test_dataset, batch_size=10, shuffle=False)
+
+
 
     import matplotlib.pyplot as plt
 
@@ -40,12 +110,29 @@ if __name__ == '__main__':
     # model = CNNModel(in_features=28 * 28, hidden_features=128, out_features=10, bias=True)
     sparse_model = SparseModel(in_features=28 * 28, hidden_features=128, out_features=10, bias=True)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    #compare_models_robustness("plot", dense_model, sparse_model)
+
     from handle_model import handle_model
+
     dense_model_runner = handle_model(dense_model, noisy_train_loader, noisy_test_loader)
     dense_model_runner.run()
+    dense_model_runner.plot_training_acc(save_string="dense_training_acc_plot.png")
 
     sparse_model_runner = handle_model(sparse_model, noisy_train_loader, noisy_test_loader)
     sparse_model_runner.run()
+    sparse_model_runner.plot_training_acc(save_string="sparse_training_acc_plot.png")
+
+    #acc_list = [dense_model_runner.training_acc, sparse_model_runner.training_acc]
+    #compare_acc(acc_list)
+
+
+
+
+
+
+
+
 
 
 
